fcee209ba5dd23f5c9b66b0e880c314b0816c07b
storm <storm@192.168.1.39>
2018-10-24 23:23:16 +0200

    rest

--- TCPLibrary/src/com/revolhope/deepdev/tcplibrary/constants/Params.java
+++ TCPLibrary/src/com/revolhope/deepdev/tcplibrary/constants/Params.java
@@ -4,4 +4,5 @@ public class Params {
 	
 	public static int PORT = 9006;
 	public static String SERVER_ADDRESS = "192.168.1.39"; // <-- TODO: Change it
+	public static long SERVER_ID = 79428792L;
 }
