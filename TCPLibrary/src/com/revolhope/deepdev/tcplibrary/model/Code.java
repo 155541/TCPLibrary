03806bbd48d61ae98cff904485b5847dd10a889e
splanes <155541.clot@fje.edu>
2018-10-30 23:08:54 +0100

    updating

--- TCPLibrary/src/com/revolhope/deepdev/tcplibrary/model/Code.java
+++ TCPLibrary/src/com/revolhope/deepdev/tcplibrary/model/Code.java
@@ -5,11 +5,12 @@ import java.io.Serializable;
 public enum Code implements Serializable
 {
 	REQ_INIT,
+	REQ_GET_DEV_CONN,
 	REQ_OPEN_SESSION,
 	REQ_TRANSMISSION,
 	REQ_CLOSE_SESSION,
 	RES_OK,
 	RES_ERROR_IO,
 	RES_ERROR_SQL,
-	RES_ERROR_NAME_IN_USE
+	RES_ERROR_NAME_IN_USE,
 }
