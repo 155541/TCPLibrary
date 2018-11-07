b02ae75a6a57fae63daa139d675556e7909fd089
splanes <155541.clot@fje.edu>
2018-11-07 21:32:50 +0100

    b

--- TCPLibrary/src/com/revolhope/deepdev/tcplibrary/constants/Params.java
+++ TCPLibrary/src/com/revolhope/deepdev/tcplibrary/constants/Params.java
@@ -3,6 +3,6 @@ package com.revolhope.deepdev.tcplibrary.constants;
 public class Params {
 	
 	public static int PORT = 9006;
-	public static String SERVER_ADDRESS = "192.168.1.39"; // <-- TODO: Change it
+	public static String SERVER_ADDRESS = "192.168.1.46"; // <-- TODO: Change it
 	public static long SERVER_ID = 79428792L; 
 }
